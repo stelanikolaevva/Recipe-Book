4a54e75ed3f125dae0b99bff4ea5c1bc8ac6b0ef
TeodoraNikolova <teodoraknikolova@abv.bg>
2021-05-16 14:47:51 +0300

    Add Models

---
+++ Recipe Book/Recipe Book/Models/Recipe.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recipe_Book.Models
+{
+    public class Recipe
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public string CookingTime { get; set; }
+        public int Servings { get; set; }
+        public string Image { get; set; }
+
+        public Recipe()
+        {
+
+        }
+    }
+}
---
+++ Recipe Book/Recipe Book/Models/RecipeBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recipe_Book.Models
+{
+    public class RecipeBook
+    {
+        public int Id { get; set; }
+        public string Publishing { get; set; }
+        public RecipeBook()
+        {
+
+        }
+    }
+}
