02400089914bd217369a5432b91e2e7b82b5fffa
stelanikolaevva <stela.nikolaeva13@gmail.com>
2021-05-16 15:31:20 +0300

    adding controllers

---
+++ Recipe Book/Recipe Book/Controllers/RecipeBooksController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Recipe_Book.Data;
+using Recipe_Book.Models;
+
+namespace Recipe_Book.Controllers
+{
+    public class RecipeBooksController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RecipeBooksController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: RecipeBooks
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.RecipeBook.ToListAsync());
+        }
+
+        // GET: RecipeBooks/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var recipeBook = await _context.RecipeBook
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (recipeBook == null)
+            {
+                return NotFound();
+            }
+
+            return View(recipeBook);
+        }
+
+        // GET: RecipeBooks/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: RecipeBooks/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Publishing")] RecipeBook recipeBook)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(recipeBook);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(recipeBook);
+        }
+
+        // GET: RecipeBooks/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var recipeBook = await _context.RecipeBook.FindAsync(id);
+            if (recipeBook == null)
+            {
+                return NotFound();
+            }
+            return View(recipeBook);
+        }
+
+        // POST: RecipeBooks/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Publishing")] RecipeBook recipeBook)
+        {
+            if (id != recipeBook.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(recipeBook);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RecipeBookExists(recipeBook.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(recipeBook);
+        }
+
+        // GET: RecipeBooks/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var recipeBook = await _context.RecipeBook
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (recipeBook == null)
+            {
+                return NotFound();
+            }
+
+            return View(recipeBook);
+        }
+
+        // POST: RecipeBooks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var recipeBook = await _context.RecipeBook.FindAsync(id);
+            _context.RecipeBook.Remove(recipeBook);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RecipeBookExists(int id)
+        {
+            return _context.RecipeBook.Any(e => e.Id == id);
+        }
+    }
+}
--- Recipe Book/Recipe Book/Data/ApplicationDbContext.cs
+++ Recipe Book/Recipe Book/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Recipe_Book.Models;
 
 namespace Recipe_Book.Data
 {
@@ -12,5 +13,9 @@ namespace Recipe_Book.Data
             : base(options)
         {
         }
+        public DbSet<Recipe_Book.Models.Ingredients> Ingredients { get; set; }
+        public DbSet<Recipe_Book.Models.Recipe> Recipe { get; set; }
+        public DbSet<Recipe_Book.Models.RecipeBook> RecipeBook { get; set; }
+        public DbSet<Recipe_Book.Models.Steps> Steps { get; set; }
     }
 }
