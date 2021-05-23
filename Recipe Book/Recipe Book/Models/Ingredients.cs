18a7abf42f8bd5195fcca7d69a90a3874742e748
stelanikolaevva <stela.nikolaeva13@gmail.com>
2021-05-23 14:19:00 +0300

    fix error

---
+++ Recipe Book/Recipe Book/Models/Ingredients.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Recipe_Book.Models
+{
+    public class Ingredients
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Quantity { get; set; }
+        public Ingredients()
+        {
+
+        }
+    }
+}
