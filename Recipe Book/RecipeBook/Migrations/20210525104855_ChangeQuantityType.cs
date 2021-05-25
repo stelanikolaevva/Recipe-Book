d0a62bdca62121c0dce1157bf2580814b2ee4da1
stelanikolaevva <stela.nikolaeva13@gmail.com>
2021-05-25 13:50:38 +0300

    fixed to show recipe name

---
+++ Recipe Book/RecipeBook/Migrations/20210525104855_ChangeQuantityType.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RecipeBook.Migrations
+{
+    public partial class ChangeQuantityType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Quantity",
+                table: "Ingredients",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Quantity",
+                table: "Ingredients",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+    }
+}
